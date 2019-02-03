39c51061f9d758b9a51771bf4331ebe5d217ad2a
Radu B <radu.buciuceanu@gmail.com>
2019-02-03 14:04:01 +0200

    Wascc.Networking.Io project was created.

---
+++ src/Wascc.Networking.Io/include/Wascc.Networking.Io/IConnection.h
@@ -0,0 +1,8 @@
+#pragma once;
+
+#include "Wascc.Networking.Io/Preprocessor.h"
+
+class WASCC_NETWORKING_IO IConnection
+{
+public:
+}
---
+++ src/Wascc.Networking.Io/include/Wascc.Networking.Io/Preprocessor.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifdef _WIN32
+
+#define IMPORT_SYMBOL __declspec(dllimport)
+#define EXPORT_SYMBOL __declspec(dllexport)
+
+#elif __GNUC__ >= 4
+
+#define IMPORT_SYMBOL __attribute__((visibility("default")))
+#define EXPORT_SYMBOL __attribute__((visibility("default")))
+
+#else
+
+#define IMPORT_SYMBOL
+#define EXPORT_SYMBOL
+
+#endif
+
+#ifdef WASCC_NETWORKING_IO
+#define WASCC_NETWORKING_IO EXPORT_SYMBOL
+#else
+#define WASCC_NETWORKING_IO IMPORT_SYMBOL
+#endif
